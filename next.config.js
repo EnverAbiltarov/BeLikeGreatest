f08739b3eeed4e75d5a97647404b23d5287634dc
eridanislyamov <eridan.islyamov@gmail.com>
2024-11-21 22:03:52 +0300

    Initial project

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = { images: {
+  remotePatterns: [
+      {
+          protocol: 'https',
+          hostname: 'files.edgestore.dev',
+      },
+  ],
+}}
+
+/*
+const nextConfig = {
+  images:{
+    domains:["files.edgestore.dev"]
+  }
+}
+*/
+module.exports = nextConfig
